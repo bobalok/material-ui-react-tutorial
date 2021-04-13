fefc2811329b70704c468e48c28fa588851e549b
findabir <findabirr@gmail.com>
2021-04-13 15:51:20 +0600

    secCommit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+
+
+const App = () => {
+    return(
+        <div>
+            <Typography>hello</Typography>
+        </div>
+    );
+}
+
+export default App;
